3b79278b6e67e513fbea9a1c1345413be9e1bd54
Marine Gomes <marine_ibac@hotmail.com>
2020-08-01 16:08:43 -0400

    Navegação Pessoas

---
+++ app/Http/Controllers/PessoaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PessoaController extends Controller
+{
+    public  function index()
+    {
+        return view('pessoa.index');
+    }
+
+    public function create()
+    {
+        return view('pessoa.create');
+    }
+}
+
+
---
+++ resources/views/pessoa/create.blade.php
@@ -0,0 +1,13 @@
+<h1>Formulario de Pessoa</h1>
+
+<form action="/pessoas" method="post">
+    <input type="text" name="nome">
+    <input type="text" name="telefone">
+    <input type="text" name="email">
+    <button type="submit">Salvar</button>
+</form>
+
+    <br>
+
+    <a href="/pessoa">Voltar</a>
+
